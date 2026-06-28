8df0bee79e8272bf7abbf684484e7f3a187a4b88
Umang Varshney <umangv.varshney@gmail.com>
2026-06-28 12:53:19 +0530

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -107,7 +107,8 @@ if not st.session_state.logged_in:
             "Role",
             [
                 "Patient",
-                "Doctor"
+                "Doctor".
+                "Admin"
             ],
             key="reg_role"
         )
